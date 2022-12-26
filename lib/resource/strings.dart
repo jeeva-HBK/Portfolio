3815d70ccc4983515c295e2a14c3c99d282b456b
jeeva <jeeva_hbk@yahoo.com>
2022-12-26 14:41:32 +0530

    changes

--- lib/resource/strings.dart
+++ lib/resource/strings.dart
@@ -57,4 +57,5 @@ class Strings {
   /* Contact screen content */
   static final endTxt =
       '''Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!''';
+
 }
